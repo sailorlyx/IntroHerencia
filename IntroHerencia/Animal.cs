2ee0269064f4bb9654b0e0eb55d3a0f741bc6814
Lili <lidiaechuacas@hotmail.com>
2022-12-14 19:59:36 +0100

    Agregar archivos de proyecto.

---
+++ IntroHerencia/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroHerencia
+{
+    internal class Animal
+    {
+        public string color = "negro";
+
+        public void uwu()
+        {
+            Console.WriteLine("soy un animalico");
+        }
+
+    }
+}
---
+++ IntroHerencia/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroHerencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Perro perrito = new Perro();
+            perrito.uwu();
+            
+            Gato gatito = new Gato();
+            gatito.uwu();
+
+            Console.WriteLine("mi perrito es de color {0} y de la raza {1}", perrito.color, perrito.raza);
+            Console.WriteLine("mi gatito es de color {0} y de la raza {1}", gatito.color, gatito.raza);
+            
+           
+        }
+    }
+}
